ce8070e160b9789a95da61606493bd057ddf2071
Waqas Ayaz <waqasayaz@icloud.com>
2025-05-25 12:57:04 +0500

    starter

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query, Res } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+    constructor(private readonly coffeeService: CoffeesService) {}
+
+    @Get()
+    findAll(@Query() paginationQuery) {
+       // const {limit, offset} = paginationQuery;
+        return this.coffeeService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number){
+        return this.coffeeService.findOne('' + id)
+    }
+
+    @Post()
+    create(@Body() createCoffeDto: CreateCoffeeDto) {
+        return this.coffeeService.create(createCoffeDto)
+    }
+
+    @Patch(':id')
+    update(@Param ('id') id: string, @Body() updateCoffeDto: UpdateCoffeeDto) {
+        return this.coffeeService.update(id,updateCoffeDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+          return this.coffeeService.remove(id)
+    }
+}
